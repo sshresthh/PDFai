2880190fae94955d21a290be79e9aab29c6e487c
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-26 07:16:55 +0930

    (updated) event listener

--- components/PlaceHolderDocument.tsx
+++ components/PlaceHolderDocument.tsx
@@ -4,8 +4,14 @@ import { PlusCircleIcon } from "lucide-react";
 import { Button } from "./ui/button";
 
 function PlaceHolderDocument() {
+  const handleClick = () => {
+    // Add your code here
+  };
   return (
-    <Button className="flex flex-col items-center justify-center w-64 h-80 rounded-xl bg-gray-200 drop-shadow-md text-gray-400">
+    <Button
+      onClick={handleClick}
+      className="flex flex-col items-center justify-center w-64 h-80 rounded-xl bg-gray-200 drop-shadow-md text-gray-400"
+    >
       <PlusCircleIcon className="h-16 w-16" />
       <p>Need to add a document</p>
     </Button>
