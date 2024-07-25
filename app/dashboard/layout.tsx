6049ddab592dc63fb208339c93e782429f4ca132
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-26 06:36:04 +0930

    (removed) card

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,13 @@
+import Header from "@/components/Header";
 import { ClerkLoaded } from "@clerk/nextjs";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkLoaded>
-      <div>{children}</div>
+      <div>
+        <Header />
+        {children}
+      </div>
     </ClerkLoaded>
   );
 }
