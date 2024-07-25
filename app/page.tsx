50ebc7c01536bd2a154ca8d20edeb4cb8487ab26
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-26 06:06:53 +0930

    (fixed) Theme & UI bugs

--- app/page.tsx
+++ app/page.tsx
@@ -66,18 +66,18 @@ export default function Home() {
           </Button>
 
           <div className="relative overflow-hidden pt-16">
-            <div className="mx-auto max-w-7xl px-1 lg:px-10">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
               <Image
                 alt="PDFai screenshot"
                 src="https://i.imgur.com/jOiN7vR.png"
                 width={2432}
                 height={1442}
-                className="mb-[-0%] rounded-xl shadow-1xl ring-1 ring-gray-900/10"
+                className="mb-[-0%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
               />
             </div>
           </div>
 
-          <div className="mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px">
+          <div className="mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
             <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:max-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
               {featuresDetails.map((feature, index) => (
                 <div key={index} className="relative pl-9">
