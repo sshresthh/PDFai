b2ecc949bd578d7567d75e35b81b31d934592b69
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-26 03:28:27 +0930

    (updated) featureDetails for the saas

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,39 @@
-import Image from "next/image";
+import {
+  GlobeIcon
+} from "lucide-react";
+
+const featuresDetails = [
+  {
+    name: "Feature 1",
+    description: "Feature 1 description",
+    icon: GlobeIcon
+  },
+  {
+    name: "Feature 2",
+    description: "Feature 2 description",
+    icon: GlobeIcon
+  },
+  {
+    name: "Feature 3",
+    description: "Feature 3 description",
+    icon: GlobeIcon
+  },
+  {
+    name: "Feature 4",
+    description: "Feature 4 description",
+    icon: GlobeIcon
+  },
+  {
+    name: "Feature 5",
+    description: "Feature 5 description",
+    icon: GlobeIcon
+  },
+  {
+    name: "Feature 6",
+    description: "Feature 6 description",
+    icon: GlobeIcon
+  }
+]
 
 export default function Home() {
   return (
