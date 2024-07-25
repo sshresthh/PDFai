9e7071b8209993c1794cd1e0f9a4bed568361dfc
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-26 07:13:21 +0930

    (added) PlaceHolderDocument to get an idea

--- components/Documents.tsx
+++ components/Documents.tsx
@@ -1,7 +1,10 @@
+import PlaceHolderDocument from "./PlaceHolderDocument";
+
 function Documents() {
   return (
     <div className="flex flex-wrap p-5 bg-gray-100 justify-center lg:justify-start rounded-sm gap-5 max-w-7xl mx-auto">
-      Documents {/*Need to map all the pdf documents*/}
+      {/*Need to map all the pdf documents*/}
+      <PlaceHolderDocument />
     </div>
   );
 }
---
+++ components/PlaceHolderDocument.tsx
@@ -0,0 +1,12 @@
+import { PlusCircleIcon } from "lucide-react";
+import { Button } from "./ui/button";
+
+function PlaceHolderDocument() {
+  return (
+    <Button className="flex flex-col items-center justify-center w-64 h-80 rounded-xl bg-gray-200 drop-shadow-md text-gray-400">
+      <PlusCircleIcon className="h-16 w-16" />
+      <p>Need to add a document</p>
+    </Button>
+  );
+}
+export default PlaceHolderDocument;
