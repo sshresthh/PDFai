19e9861a204ab206057b1647c4326026196a98f4
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-26 07:08:37 +0930

    (added) Documents components (mapping is left)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,12 @@
+import Documents from "@/components/Documents";
+
 function Dashboard() {
   return (
     <div className="h-full max-w-7xl mx-auto">
       <h1 className="text-3xl p-5 bg-gray-100 font-light text-blue-500">
         Documents
       </h1>
+      <Documents />
     </div>
   );
 }
---
+++ components/Documents.tsx
@@ -0,0 +1,8 @@
+function Documents() {
+  return (
+    <div className="flex flex-wrap p-5 bg-gray-100 justify-center lg:justify-start rounded-sm gap-5 max-w-7xl mx-auto">
+      Documents {/*Need to map all the pdf documents*/}
+    </div>
+  );
+}
+export default Documents;
