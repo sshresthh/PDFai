7b7fcc20acb30d031241e5cb13e101f3a605a596
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-26 07:05:15 +0930

    (updated) forced-dynamic (always server side rendered)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,10 @@
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="h-full max-w-7xl mx-auto">
+      <h1 className="text-3xl p-5 bg-gray-100 font-light text-blue-500">
+        Documents
+      </h1>
+    </div>
+  );
 }
 export default Dashboard;
