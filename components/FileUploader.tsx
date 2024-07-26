4c5776db0940b609d536ab6d8d21572804ab540f
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-26 22:40:58 +0930

    (updated) the styles of the file upload

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,6 +1,31 @@
 "use client";
 
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
 function FileUploader() {
-  return <div>FileUploader</div>;
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive, isFocused } = useDropzone({
+    onDrop,
+  });
+  return (
+    <div className="flex flex-col gap-4 items-center max-w-7xl mx-auto">
+      <div
+        {...getRootProps()}
+        className={`p-10 border-2 border-dashed m-10 w-[90%] border-blue-600 text-blue-600`}
+      >
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>
+            Drag &rsquo;n&rsquo; drop some files here, or click to select files
+          </p>
+        )}
+      </div>
+    </div>
+  );
 }
 export default FileUploader;
