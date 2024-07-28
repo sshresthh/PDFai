8a142a5dc334cf5eef611208be1446cc18e4aab2
ss.stha00 <ss.stha00@gmail.com>
2024-07-28 05:46:54 +0000

    (updated) enum type status

--- hooks/useUpload.ts
+++ hooks/useUpload.ts
@@ -1,6 +1,15 @@
 'use client'
 import {useState} from 'react';
 
+export enum StatusText {
+    UPLOADING = "Uploading file...",
+    UPLOADED = "File uploaded successfully",
+    SAVING = "Saving file to database...",
+    GENERATING = "Gnerating the AI vector Embeddings...please wait for few seconds..."
+}
+
+export type Status = StatusText[keyof StatusText];
+
 function useUpload() {
     const [progress, setProgress] = useState<number | null>(null);
     const [fileId, setFileId] = useState<string | null>(null);
