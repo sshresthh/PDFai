5e6e4a4762dc5a624579138abebe52ebc6ef1b0c
ss.stha00 <ss.stha00@gmail.com>
2024-07-28 04:03:03 +0000

    (added) firebase

---
+++ firebase.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXx6LVL0hhIB3r7u4D6OAIbne9jxyUdj4",
+    authDomain: "pdfai-sshresthh.firebaseapp.com",
+    projectId: "pdfai-sshresthh",
+    storageBucket: "pdfai-sshresthh.appspot.com",
+    messagingSenderId: "376560166055",
+    appId: "1:376560166055:web:b1987ff1c312a534e82782",
+    measurementId: "G-7E9FJD793K"
+  };
