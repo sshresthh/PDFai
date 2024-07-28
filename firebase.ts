62f1699708519e1f83483a7f5610aa61be074132
ss.stha00 <ss.stha00@gmail.com>
2024-07-28 05:10:14 +0000

    (updates) firebase configurations

--- firebase.ts
+++ firebase.ts
@@ -1,3 +1,9 @@
+import {getApp, getApps, initializeApp} from "firebase/app"
+import {getFirestore} from "firebase/firestore"
+import {getStorage} from "firebase/storage"
+import { getAnalytics } from "firebase/analytics";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCXx6LVL0hhIB3r7u4D6OAIbne9jxyUdj4",
     authDomain: "pdfai-sshresthh.firebaseapp.com",
@@ -7,3 +13,11 @@ const firebaseConfig = {
     appId: "1:376560166055:web:b1987ff1c312a534e82782",
     measurementId: "G-7E9FJD793K"
   };
+
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+
+export {db, storage, analytics};
